3809d9b0847bd046bbe3caca46addc68447f2ae8
Luowan Xu <2710660X@student.gla.ac.uk>
2023-02-08 03:02:45 +0000

    eighth commit

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+
+
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
                            help_text="Please enter the category name.")
@@ -12,6 +14,7 @@ class CategoryForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Category
         fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
@@ -32,3 +35,14 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         # or specify the fields to include (don't include the category field).
         #fields = ('title', 'url', 'views')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
--- rango/models.py
+++ rango/models.py
@@ -27,3 +27,6 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     def __str__(self):
         return self.title
+
+
+    
